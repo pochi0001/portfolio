539d8c1a80538c7594357963214ba0787691b6f0
pochi0001 <pochi00kou00@gmail.com>
2026-03-26 10:45:14 +0900

    add portfolio click events

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// Googleアナリティクス イベント送信
-function trackEvent(action, label) {
-  gtag('event', action, {
+function trackEvent(label) {
+  gtag('event', 'portfolio_click', {
     event_category: 'click',
     event_label: label
   });
